8958cf847d6df50566a3bdce86d5124e788754d8
Your Name <ndeanazira@yahoo.com>
2025-11-11 03:42:25 +0000

    Files re-added.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var ejs = require("ejs");
 // const bubbles = require("./assets/bubble_list.json")
 var navData = require("./assets/nav-data.json");
-var bubbles = navData.filter(function (item) { return item.name == "Pages"; })[0].dropdown;
+var bubbles = navData.find(function (item) { return item.name == "Pages"; }).dropdown;
 // console.log(bubbles);
 var ejsLayout = require("express-ejs-layouts");
 var app = express();
@@ -11,11 +11,17 @@ app.use('/assets', express.static("assets"));
 app.use(ejsLayout);
 app.set("view engine", "ejs");
 app.get("/", function (req, res) {
-    res.render("home", { title: "Home", bubbles: bubbles, cssLinkPath: null, navData: navData });
+    var url = req.url;
+    res.render("home", { title: "Home", bubbles: bubbles, cssLinkPath: null, navData: navData, url: url });
 });
 app.get("/tasks/:taskPageName", function (req, res) {
     var taskPageName = req.params.taskPageName;
-    res.render("Tasks/" + taskPageName.toLowerCase(), { title: taskPageName, cssLinkPath: "/assets/" + taskPageName + "/styling.css", navData: navData });
+    var url = req.url;
+    res.render("Tasks/" + taskPageName.toLowerCase(), { title: taskPageName, cssLinkPath: "/assets/" + taskPageName + "/styling.css", navData: navData, url: url });
+});
+app.get("/about", function (req, res) {
+    var url = req.url;
+    res.render("about", { title: "About", cssLinkPath: null, bubbles: bubbles, navData: navData, url: url });
 });
 app.listen(port, function () {
     console.log("Server is now running at port: " + port);
