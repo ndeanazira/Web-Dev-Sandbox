d5d0c6fd61d6421eb59e683e06a93a0a3ee820f9
N-Dean <ndeanazira@yahoo.com>
2026-05-27 04:18:35 +0100

    Added portfolio page 'About Me' and added more sass and some light colors, as well as margin classes.

--- index.js
+++ index.js
@@ -24,18 +24,14 @@ app.get("/about", function (req, res) {
     var url = req.url;
     res.render("about", { title: "About", cssLinkPath: null, bubbles: bubbles, navData: navData, url: url });
 });
-<<<<<<< HEAD
-app.get("/about/me", (req, res) => {
-    const { url } = req;
-    res.render("about-this-site", { title: "About Me", cssLinkPath: null, bubbles, navData, url });
-});
-app.listen(port, () => {
-=======
 app.get("/about-me", function (req, res) {
     var url = req.url;
     res.render("about-me", { title: "About Me", cssLinkPath: null, bubbles: bubbles, navData: navData, url: url });
 });
+app.get("/about/me", function (req, res) {
+    var url = req.url;
+    res.render("about-this-site", { title: "About Me", cssLinkPath: null, bubbles: bubbles, navData: navData, url: url });
+});
 app.listen(port, function () {
->>>>>>> 88935f65aedd5f679e9253d09d2b3e7a93369416
     console.log("Server is now running at port: " + port);
 });
